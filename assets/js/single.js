112bb1e724f26e067b9b80fadd7753bd029664b0
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-24 18:43:42 -0700

    added warning for more than 30 repos

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo){
     console.log(repo);
@@ -12,6 +13,11 @@ var getRepoIssues = function(repo){
             });
         } else {
             alert("There was a problem with your request!");
+        }
+
+        // chec kif api has paginated issues
+        if (response.headers.get("Link")){
+            displayWarning(repo);
         }
     });
 }
@@ -51,5 +57,17 @@ var displayIssues = function(issues){
         issueContainerEl.appendChild(issueEl);
     }
 
+}
+
+var displayWarning = function(repo){
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 }
 getRepoIssues("facebook/react");
